c51895c4fcfe292c1c16d617c1e4da404436ffc0
Nathan Hunt <nathan.taylor.hunt@gmail.com>
2024-06-12 13:04:52 -0400

    remove text input
    should probably disable the submit key too

--- src/ui.rs
+++ src/ui.rs
@@ -96,42 +96,42 @@ pub(crate) fn render_ui(
             // Since immediate mode ui can't predict the final sizes of widgets until they've already been drawn
 
             // Thus we create a bottom panel first, where our text edit and submit button resides.
-            egui::TopBottomPanel::bottom("bottom panel")
-                .frame(egui::Frame::none().outer_margin(egui::Margin {
-                    left: 0.0,
-                    right: 5.0,
-                    top: 5. + 6.,
-                    bottom: 5.0,
-                }))
-                .show_inside(ui, |ui| {
-                    let text_edit_id = egui::Id::new("text_edit");
+            // egui::TopBottomPanel::bottom("bottom panel")
+            //     .frame(egui::Frame::none().outer_margin(egui::Margin {
+            //         left: 0.0,
+            //         right: 5.0,
+            //         top: 5. + 6.,
+            //         bottom: 5.0,
+            //     }))
+            //     .show_inside(ui, |ui| {
+            //         let text_edit_id = egui::Id::new("text_edit");
 
-                    //We can use a right to left layout, so we can place the text input last and tell it to fill all remaining space
-                    ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
-                        if ui.button("Submit").clicked() {
-                            submit_command(&mut state.command);
+            //         //We can use a right to left layout, so we can place the text input last and tell it to fill all remaining space
+            //         ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+            //             if ui.button("Submit").clicked() {
+            //                 submit_command(&mut state.command);
 
-                            // Return keyboard focus to the text edit control.
-                            ui.ctx().memory_mut(|mem| mem.request_focus(text_edit_id));
-                        }
-                        // ui.button is a shorthand command, a similar command exists for text edits, but this is how to manually construct a widget.
-                        // doing this also allows access to more options of the widget, rather than being stuck with the default the shorthand picks.
-                        let text_edit = egui::TextEdit::singleline(&mut state.command)
-                            .id(text_edit_id)
-                            .desired_width(ui.available_width())
-                            .margin(egui::Vec2::splat(4.0))
-                            .font(config.theme.font.clone())
-                            .lock_focus(true);
+            //                 // Return keyboard focus to the text edit control.
+            //                 ui.ctx().memory_mut(|mem| mem.request_focus(text_edit_id));
+            //             }
+            //             // ui.button is a shorthand command, a similar command exists for text edits, but this is how to manually construct a widget.
+            //             // doing this also allows access to more options of the widget, rather than being stuck with the default the shorthand picks.
+            //             let text_edit = egui::TextEdit::singleline(&mut state.command)
+            //                 .id(text_edit_id)
+            //                 .desired_width(ui.available_width())
+            //                 .margin(egui::Vec2::splat(4.0))
+            //                 .font(config.theme.font.clone())
+            //                 .lock_focus(true);
 
-                        ui.add(text_edit);
+            //             ui.add(text_edit);
 
-                        // Each time we open the console, we want to set focus to the text edit control.
-                        if !state.text_focus {
-                            state.text_focus = true;
-                            ui.ctx().memory_mut(|mem| mem.request_focus(text_edit_id));
-                        }
-                    });
-                });
+            //             // Each time we open the console, we want to set focus to the text edit control.
+            //             if !state.text_focus {
+            //                 state.text_focus = true;
+            //                 ui.ctx().memory_mut(|mem| mem.request_focus(text_edit_id));
+            //             }
+            //         });
+            //     });
             // Now we can fill the remaining minutespace with a scrollarea, which has only the vertical scrollbar enabled and expands to be as big as possible.
             egui::ScrollArea::new([false, true])
                 .auto_shrink([false, true])
